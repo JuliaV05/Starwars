af61ab6a14852bc6e62695f7aaab97acf420ffa3
Usuário <juliavianamendes05@gmail.com>
2023-01-20 15:01:22 -0300

    starting the project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <span>Hello, Júlia! Vamos lá ? 
+      O Senhor te capacitou para fazer esse projeto. Ele acredita em você!
+    </span>
   );
 }
 
